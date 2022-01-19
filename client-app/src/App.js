745161abc508de555cded6271a416c101ba56477
mihai10001 <rvivits@gmail.com>
2022-01-19 16:03:25 +0200

    add all state variables, get code from query params, get access token and user

--- client-app/src/App.js
+++ client-app/src/App.js
@@ -1,8 +1,13 @@
 import React, { useState, useEffect, useMemo } from 'react';
+import { useSearchParams, useNavigate } from 'react-router-dom';
 import './App.css';
 
 
 function App() {
+  const [searchParams] = useSearchParams();
+  const [urlCode, setUrlCode] = useState('');
+  const [token, setToken] = useState({ accessToken: '', refreshToken: '' });
+  const [user, setUser] = useState({ userName: '', userId: '', userEmail: '' });
   const DiscordOauth2 = require("discord-oauth2");
 
   const clientId = process.env.REACT_APP_CLIENT_ID;
@@ -16,6 +21,32 @@ function App() {
     }), [clientId, clientSecret, clientRedirect, DiscordOauth2]
   );
 
+  useEffect(() => {
+    const code = searchParams.get('code');
+    if (code && !urlCode) setUrlCode(code);
+  }, [searchParams, urlCode]);
+
+  useEffect(() => {
+    if (urlCode)
+      oauth.tokenRequest({
+        clientId: clientId,
+        clientSecret: clientSecret,
+        code: urlCode,
+        scope: "identify email guilds",
+        grantType: "authorization_code",
+        redirectUri: clientRedirect,
+      })
+      .then(res => setToken({accessToken: res.access_token, refreshToken: res.refresh_token}))
+      .catch(error => console.log(error));
+  }, [oauth, clientId, clientSecret, clientRedirect, urlCode]);
+
+  useEffect(() => {
+    if (token.accessToken)
+      oauth.getUser(token.accessToken)
+      .then(res => setUser({userName: res.username, userId: res.id, userEmail: res.email}))
+      .catch(error => console.log(error));
+  }, [oauth, token, setUser]);
+  
   return (
     <div className="App">
       <header className="App-header">
