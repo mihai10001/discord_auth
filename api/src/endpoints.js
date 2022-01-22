48162c61c118b7adcf998d4c1ed720abab2a67c8
mihai10001 <rvivits@gmail.com>
2022-01-22 21:49:49 +0200

    authentication server-side implementation

--- api/src/endpoints.js
+++ api/src/endpoints.js
@@ -18,19 +18,50 @@ module.exports = (app, dbClient) => {
   });
 };
 
-async function validateData(userId, userName, userDiscriminator, wallet, dbClient) {
-  if (!userId || !userName || !userDiscriminator || !wallet) throw 'MISSING FIELDS';
+async function authenticateUser(code) {
+  if (!code) throw 'MISSING FIELDS';
+
+  const oauthResult = await fetch('https://discord.com/api/oauth2/token', {
+    method: 'POST',
+    body: new URLSearchParams({
+      client_id: clientId,
+      client_secret: clientSecret,
+      code,
+      redirect_uri: clientRedirect,
+      scope: 'identify',
+      grant_type: 'authorization_code'
+    }),
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded',
+    },
+  });
+
+  return oauthResult.json();
+}
+
+async function getUserInformation(oauthData) {
+  if (!oauthData) throw 'MISSING FIELDS';
+
+  const userData = await fetch('https://discord.com/api/users/@me', {
+	  headers: { authorization: `${oauthData.token_type} ${oauthData.access_token}` },
+  });
+
+  return userData.json();
+}
+
+async function isUserInServer(userData) {
+  if (!userData) throw 'MISSING FIELDS';
 
   const client = new Client({ intents: [Intents.FLAGS.GUILD_MEMBERS] });
   await client.login(discordBotToken);
   const list = client.guilds.cache.get(discordGuildId);
   const members = await list.members.fetch();
-  const member = members.find(member => member.user.id === userId);
+  const member = members.find(member => member.user.id === userData.id);
 
   if (member) {
     const user = member.user;
-    if (user.id === userId && user.username === userName && user.discriminator === userDiscriminator) {
-      await dbClient.collection(dbCollectionName).insertOne({ userId, userName, userDiscriminator, wallet, createdOn: new Date() });
+    if (user.id === userData.id && user.username === userData.username && user.discriminator === userData.discriminator) {
+      return true;
     } else throw 'INCONSISTENT DATA';
   } else throw 'MEMBER IS NOT IN SERVER';
 }
